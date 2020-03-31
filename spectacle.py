f79461eab5d7193a5986750ab7cda38db5f32b41
freder <freder.code@gmail.com>
2020-03-31 18:03:20 +0200

    left / right

--- spectacle.py
+++ spectacle.py
@@ -10,21 +10,59 @@ height = int(height)
 
 cmd = args[1]
 
+removeMaximized = 'wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz'
+addMaximizedVert = 'wmctrl -r :ACTIVE: -b add,maximized_vert'
+
+def commaSeparated(arr):
+	return ','.join([
+			str(int(abs(a))) for a in arr
+		]
+	)
+
+
 if (cmd == 'center'):
 	w = 0.5 * width
 	h = 0.7 * height
 	x = (w - width) * 0.5
 	y = (h - height) * 0.5
 	wmctrl = 'wmctrl -r :ACTIVE: -e \'{}\''.format(
-		','.join([
-			str(int(abs(a))) for a in [0, x, y, w, h]
-		])
+		commaSeparated([0, x, y, w, h])
 	)
 	print(
 		' ; '.join([
-			'wmctrl -r :ACTIVE: -b remove,maximized_vert,maximized_horz',
+			removeMaximized,
 			wmctrl
 		])
 	)
 elif (cmd == 'maximize'):
 	print('wmctrl -r :ACTIVE: -b add,maximized_vert,maximized_horz')
+elif (cmd == 'right'):
+	w = 0.5 * width
+	h = height
+	x = width - w
+	y = 0
+	wmctrl = 'wmctrl -r :ACTIVE: -e \'{}\''.format(
+		commaSeparated([0, x, y, w, h])
+	)
+	print(
+		' ; '.join([
+			removeMaximized,
+			wmctrl,
+			addMaximizedVert,
+		])
+	)
+elif (cmd == 'left'):
+	w = 0.5 * width
+	h = height
+	x = 0
+	y = 0
+	wmctrl = 'wmctrl -r :ACTIVE: -e \'{}\''.format(
+		commaSeparated([0, x, y, w, h])
+	)
+	print(
+		' ; '.join([
+			removeMaximized,
+			wmctrl,
+			addMaximizedVert,
+		])
+	)
